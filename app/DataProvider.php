57ed7e09000d7a657ea6439d167d95b7e6ec0014
Marek Sommer <marek.sommer.1@gmail.com>
2023-12-05 10:48:48 +0100

    Add DataProvider

---
+++ app/DataProvider.php
@@ -0,0 +1,30 @@
+<?php
+  class DataProvider {
+    private string $json;
+    private array $json_data;
+
+    public function __construct(string $filePath) {
+      try {
+        $this->readJson($filePath);
+      } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+      }
+    }
+
+    private function readJson(string $filePath): void {
+      if (!file_exists($filePath))
+        throw new Exception('File not found');
+
+      $this->json = file_get_contents($filePath);
+      $this->json_data = json_decode($this->json, true);
+    }
+
+    public function getJson(): string {
+      return $this->json;
+    }
+
+    public function getJsonData(): array {
+      return $this->json_data;
+    }
+  }
+?>
